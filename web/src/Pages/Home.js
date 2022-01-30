6c81dd8a317c5a5a8273147e82eebf1776dc3248
rahulkesharwani353 <rahulkesharwani353@gmail.com>
2022-01-30 12:39:22 +0530

    bg added

--- web/src/Pages/Home.js
+++ web/src/Pages/Home.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import NavBar from '../Utils/NavBar';
-import bg1 
+import bg1 from '../accets/bh1.png';
 
 const Home = () => {
     return (
         <div>
           <NavBar/>
           <div>
-              <img src=
+              <img src={bg1} alt="bg1" className="bg1" style={{width:'100%'}}/>
           </div>
           </div>
     );
